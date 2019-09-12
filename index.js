05cf4f9275dd43d278c384f448e626a5bfe581f5
schazbot <charlotte.pearce1984@gmail.com>
2019-09-12 11:01:33 +0100

    trnasport buttong stopping sequence phrase

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-// window.addEventListener('keydown', function (e) {
-//     const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-//     const key = document.querySelector(`.pad[data-key="${e.keyCode}"]`)
+window.addEventListener('keydown', function (e) {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.pad[data-key="${e.keyCode}"]`)
 
-//     if (!sound) return
-//     sound.currentTime = 0
-//     sound.play()
-//     key.classList.add('playing')
+    if (!sound) return
+    sound.currentTime = 0
+    sound.play()
+    key.classList.add('playing')
 
-//     function removeTransition(e) {
-//         if (e.propertyName !== 'transform') return
-//         console.log(e.propertyName)
-//         this.classList.remove('playing')
-//     }
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return
+        console.log(e.propertyName)
+        this.classList.remove('playing')
+    }
 
-//     const pads = document.querySelectorAll('.pad')
-//     pads.forEach(pad => pad.addEventListener('transitionend', removeTransition))
+    const pads = document.querySelectorAll('.pad')
+    pads.forEach(pad => pad.addEventListener('transitionend', removeTransition))
+})
 
-
-
-
-// })
-
-
+var context = new AudioContext();
 
 function sequencer() {
     const kick = new Tone.Player("./808/BD7550.WAV").toDestination()
@@ -32,8 +28,13 @@ function sequencer() {
     Tone.Transport.scheduleRepeat(repeat, "16n")
     Tone.Transport.start()
 
+    const startStopButton = document.querySelector('#startStop').addEventListener('click', function() {
+        Tone.Transport.stop(() => {
+          console.log('Playback resumed successfully');
+        });
+      });
 
-    
+
 
 
     function repeat() {
@@ -45,12 +46,12 @@ function sequencer() {
         }
 
         let snareInputs = document.querySelector(`.snare input:nth-child(${step + 1})`)
-        if (snareInputs.checked) { 
+        if (snareInputs.checked) {
             snare.start()
         }
 
         let hiHatInputs = document.querySelector(`.hi-hat input:nth-child(${step + 1})`)
-        if (hiHatInputs.checked) { 
+        if (hiHatInputs.checked) {
             hiHat.start()
         }
 
@@ -58,14 +59,25 @@ function sequencer() {
     }
 }
 
-// function startStop() { 
-//     const startStopButton = document.querySelector(".transport")
-//     if (startStopButton.checked) {
-//         Tone.Transport.stop()
+// function startStop() {
+//     const startStopButton = document.querySelector("#startStop")
+//     startStopButton.addEventListener('click', () => {
+//         Tone.Transport.start()
+
+//         var context = new AudioContext();
+//         context.resume().then(() => console.log("startStopButton clicked"))
 //     }
 
+//     )
+
 // }
-sequencer()
+
+function init() {
+    sequencer()
+    // startStop()
+}
+
+init()
 
 
 
